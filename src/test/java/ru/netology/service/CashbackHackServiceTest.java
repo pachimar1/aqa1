682c984ae9f9984cfe25eab3e0c44e98f9978e80
LeM <mihail.lebedev88@gmail.com>
2023-06-15 16:56:16 +0600

    junit4 and jupiter

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -13,7 +13,7 @@ public class CashbackHackServiceTest {
         int expected = 500;
         int actual = service.remain(amount);
 
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
     }
     @org.junit.Test
     public void testForDownBorder() {
@@ -23,10 +23,10 @@ public class CashbackHackServiceTest {
         int expected = 1_000;
         int actual = service.remain(amount);
 
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
     }
 
-    @org.junit.Test
+    @org.junit.jupiter.api.Test
     public void testForUpBorder() {
         CashbackHackService service = new CashbackHackService();
 
@@ -34,6 +34,37 @@ public class CashbackHackServiceTest {
         int expected = 0;
         int actual = service.remain(amount);
 
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
+    }
+    @org.junit.jupiter.api.Test
+    public void jupiterTestRemainWithPositiveValue() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 500;
+        int expected = 500;
+        int actual = service.remain(amount);
+
+        Assert.assertEquals(expected, actual);
+    }
+    @org.junit.jupiter.api.Test
+    public void jupiterTestForDownBorder() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 0;
+        int expected = 1_000;
+        int actual = service.remain(amount);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void jupiterTestForUpBorder() {
+        CashbackHackService service = new CashbackHackService();
+
+        int amount = 1_000;
+        int expected = 0;
+        int actual = service.remain(amount);
+
+        Assert.assertEquals(expected, actual);
     }
 }
